34dbabf119db6bd9096acaa149e305998f7d9e80
Zadkiel Aslafy-Aharonian <aslafy_z@epitech.eu>
2016-12-07 11:47:52 +0000

    fixed toc

--- src/EpubView/EpubView.js
+++ src/EpubView/EpubView.js
@@ -1,5 +1,5 @@
 import React, {Component, PropTypes} from 'react'
-import Epub from 'epubjs-es6'
+import Epub from 'epubjs'
 import defaultStyles from './style'
 
 global.ePub = Epub // Fix for v3 branch of epub.js -> needs ePub to by a global var
